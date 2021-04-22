5546e9b65ab828a398343eb529e5171286ebd8aa
Matt Mallon <mattmallon91@gmail.com>
2021-04-22 13:45:52 -0600

    Tweaks to droppable locations

--- drag-and-drop/src/app/app.component.ts
+++ drag-and-drop/src/app/app.component.ts
@@ -31,11 +31,11 @@ export class AppComponent {
         {id: 1, type: "DRAGGABLE", count: 1, width: 48, height: 100, imgUrl: './assets/q1-option1.png', left: null, top: null, answer_id: null },
         {id: 2, type: "DRAGGABLE", count: 3, width: 46, height: 100, imgUrl: './assets/q1-option2.png', left: null, top: null, answer_id: null },
         {id: 5, type: "DRAGGABLE", count: 1, width: 143, height: 100, imgUrl: './assets/q1-option3-resized2.png', left: null, top: null, answer_id: null },
-        {id: 3, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 80, top: 25, answer_id: 5 },
+        {id: 3, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 85, top: 35, answer_id: 5 },
         {id: 4, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 20, top: 130, answer_id: 2 },
         {id: 6, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 86, top: 238, answer_id: 1 }, 
-        {id: 7, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 212, top: 238, answer_id: 2 }, 
-        {id: 8, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 275, top: 130, answer_id: 2 }
+        {id: 7, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 213, top: 238, answer_id: 2 }, 
+        {id: 8, type: "DROPPABLE", count: 1, width: 80, height: 120, imgUrl: null, left: 275, top: 133, answer_id: 2 }
       ]
     };
     //original image/dimensions for option 3, which is the tricky one, if we decide to go back to the original:
